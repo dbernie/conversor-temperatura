9de63cabbb08ce25d89b62eec9c4697893e70c66
Diogo Mendes <kdiogo354@gmail.com>
2026-06-09 18:53:23 -0300

    Update kelvin.py to format temperature conversions
    Refactor temperature conversion output format to display in a table.

--- kelvin.py
+++ kelvin.py
@@ -3,9 +3,15 @@ kelvin = (float(input("Digite temperatura em Kelvin: ")))
 
 
 def resultado_kelvin():
-# Convertendo °K para °C
+
+    print("-" * 54)
+    print(f"|{'Kelvin (°K)':<15} | {'Celsius (°C)':<15} | {'Fahrenheit (°F)':<15} |")
+    print("-" * 54)
+
     celsius = kelvin - 273.15
-    print(f"{kelvin:.2f}°K convertido para Celsius: {celsius:.2f}°C")
-# Convertendo °K para °F
     fahrenheit = 1.8 * (kelvin - 273.15) + 32
-    print(f"{kelvin:.2f}°K convertido para Fahrenheit: {fahrenheit:.2f}°F")
+
+    print(f"|{kelvin:<15.2f} | {celsius:<15.2f} | {fahrenheit:<15.2f} |")
+    print("-" * 54)
+
+
