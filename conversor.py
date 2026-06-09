fd51b7135386f8693b05f1d97782647b9b1a9eb0
Diogo Mendes <kdiogo354@gmail.com>
2026-06-09 19:03:39 -0300

    atualizando conversor.py
    adicionando funcionalidade nova de incluir tabelas com varios valores numericos para conversão e formatando visual.

--- conversor.py
+++ conversor.py
@@ -1,14 +1,30 @@
-options = print("1 - Celsius\n2 - Kelvin\n3 - Fahrenheit")
-choice = (int(input("Digite a escala base que deseja converter: ")))
 
-if choice == 1:
-    from celsius import resultado_celsius as resultado_c
-    resultado_c()
-elif choice == 2:
-    from kelvin import resultado_kelvin as resultado_k
-    resultado_k()
-elif choice == 3:
-    from fahrenheit import resultado_fahrenheit as resultado_f
-    resultado_f()
-else:
-    print("Tente novamente")
+def conversion():
+
+        qtd = input("Converter Tabela (S/n): ").lower()
+        if qtd == "s":
+            from tabelas.tabela import tbls
+            tbls()
+            return 
+        elif qtd == "n":
+
+            options = print("1 - Celsius\n2 - Kelvin\n3 - Fahrenheit")
+            choice = (int(input("Digite a escala base que deseja converter: ")))
+
+
+            if choice == 1:
+                from escalas.celsius import resultado_celsius as resultado_c
+                resultado_c()
+            elif choice == 2:
+                from escalas.kelvin import resultado_kelvin as resultado_k
+                resultado_k()
+            elif choice == 3:
+                from escalas.fahrenheit import resultado_fahrenheit as resultado_f
+                resultado_f()
+            else:
+                print("ERROR!! Tente novamente!")
+
+        else:
+            print("ERROR!! Tente novamente!")
+
+conversion()
