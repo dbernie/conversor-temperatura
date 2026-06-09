ece779e43b9b4686ad65835cb4e455f373f10c51
Diogo Mendes <kdiogo354@gmail.com>
2026-06-09 18:53:42 -0300

    Enhance output formatting for temperature conversions
    Added formatted output for temperature conversion results.

--- celsius.py
+++ celsius.py
@@ -3,9 +3,15 @@ celsius = (float(input("Digite temperatura em Celsius: ")))
 
 
 def resultado_celsius():
-    # Convertendo °C para °K
+    print("-" * 54)
+    print(f"|{'Celsius (°C)':<15} | {'Kelvin (°K)':<15} | {'Fahrenheit (°F)':<15} |")
+    print("-" * 54)
+
     kelvin = celsius + 273.15
-    print(f"{celsius:.2f}°C convertido para Kelvin: {kelvin:.2f}°K")
-# Convertendo °C para °F
     fahrenheit = celsius * 1.8 + 32
-    print(f"{celsius:.2f}°C convertido para Fahrenheit: {fahrenheit:.2f}°F")
+
+    print(f"|{celsius:<15.2f} | {kelvin:<15.2f} | {fahrenheit:<15.2f} |")
+    print("-" * 54)
+
+
+
